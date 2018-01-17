0b5b5aeb2a591b0d724d7a3cd04cd3797a1e2cb6
stevenjburns@msn.com <stevenjburns@msn.com>
2018-01-17 14:14:58 -0700

    Home Page CSS fixed for paragraph opactiy and border radius

--- src/pages/pageHome.js
+++ src/pages/pageHome.js
@@ -11,8 +11,8 @@ class PageHome extends Component {
   render() {
     return (
       <main className='page-home' style={{backgroundImage: `url(${Background})`}}>
-        <div>
-          <p className='div-home-content'>TwoFifteen is an open source database and API referencing all things related to professional baseball.  The data is comprehensive dating back to 1871 covering the ballparks (venues), franchises (teams), players and game details. </p>
+        <div className='div-page-content'>
+          <p className='p-home-content'>TwoFifteen is an open source database and API referencing all things related to professional baseball.  The data is comprehensive dating back to 1871 covering the ballparks (venues), franchises (teams), players and game details. </p>
         </div>
       </main>
     );
