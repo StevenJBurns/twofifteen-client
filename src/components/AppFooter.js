04b48a4f7c846de37a77a8e6c80afab96e8ede5d
stevenjburns@msn.com <stevenjburns@msn.com>
2018-01-21 03:25:55 -0700

    Added Retrosheet Data Disclaimer to app footer and tweaked the CSS

--- src/components/AppFooter.js
+++ src/components/AppFooter.js
@@ -11,6 +11,7 @@ class Footer extends Component {
     return (
       <footer className="App-footer">
         <h4>&copy; 2018 Steven J Burns</h4>
+        <p>The information used here was obtained free of charge from and is copyrighted by Retrosheet.  Interested parties may contact Retrosheet at "www.retrosheet.org".</p>
       </footer>
     );
   }
