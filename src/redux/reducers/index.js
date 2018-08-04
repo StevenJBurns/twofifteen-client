18b71933221dd59d090445de26f80b1e9d883866
StevenJBurns <stevenjburns@msn.com>
2018-08-03 23:55:11 -0600

    Copied boilerplate for actions from ExtraSolar project; will change details to TwoFifteen actions later

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,1 +1,16 @@
-import redux from "redux";
+const initialState = {
+    
+};
+
+const rootReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "TOGGLE_AUDIO":
+      return { ...state, isAudioMuted: !action.payload };
+    case "CHANGE_AUDIO_SOURCE":
+      return { ...state, currentAudioSource: action.payload };
+    default:
+      return state;
+  }
+}; 
+
+export default rootReducer;
