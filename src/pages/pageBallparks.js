7682b97619fca84d231f14bc7a035b2dd2cdb9bc
StevenJBurns <stevenjburns@msn.com>
2018-08-27 23:53:42 -0600

    Tweaking React.Component import on PageBallparks

--- src/pages/pageBallparks.js
+++ src/pages/pageBallparks.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import Background from '../images/ballparks.jpg';
 import Anaheim from '../images/anaheim.jpg';
@@ -31,7 +31,7 @@ import Atlanta from '../images/atlanta.jpg';
 import ChicagoN from '../images/chicago-n.jpg';
 import Bronx from '../images/bronx.jpg';
 
-class PageBallparks extends Component {
+class PageBallparks extends React.Component {
   static api_url = "http://twofifteen-api.azurewebsites.net/venue";
 
   constructor(props) {
