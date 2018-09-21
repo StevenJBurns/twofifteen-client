41098b654bc861ac0c712b3726b38d9ce8a047e0
StevenJBurns <stevenjburns@msn.com>
2018-09-20 20:56:17 -0600

    Starting serious CSS refactoring -- moving PageHome styles to it's own file -- more to follow

--- src/pages/PageHome.jsx
+++ src/pages/PageHome.jsx
@@ -2,6 +2,7 @@
 import React from 'react';
 
 /* Stylesheets */
+import "../styles/containers/PageHome.css";
 
 /* Assets */ 
 import Background from '../images/home.jpg';
@@ -15,7 +16,7 @@ class PageHome extends React.Component {
 
   render() {
     return (
-      <main className='page-home' style={{backgroundImage: `url(${Background})`}}>
+      <main className='main-home' style={{backgroundImage: `url(${Background})`}}>
         <div className='div-page-content'>
           <p className='p-home-content'>TwoFifteen is an open source database and API referencing all things related to professional baseball.  The data is comprehensive dating back to 1871 covering the ballparks (venues), franchises (teams), players and game details. </p>
         </div>
