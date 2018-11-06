838685b73f78546bdedcabce1a5ac2173ecc33dc
StevenJBurns <stevenjburns@msn.com>
2018-11-05 23:19:53 -0700

    Created a boilerplate AppMain functional component

--- src/components/app/AppFooter.jsx
+++ src/components/app/AppFooter.jsx
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class AppFooter extends Component {
+class AppFooter extends React.Component {
 
   constructor(props) {
     super(props);
---
+++ src/components/app/AppMain.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default AppMain = () => {
+  return (
+    <main className="app-main"></main>
+  );
+};
