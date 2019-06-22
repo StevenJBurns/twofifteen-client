2812bf618a4b5c8b6cb67782f80bdbee7a8a721e
StevenJBurns <stevenjburns@msn.com>
2019-06-22 08:35:42 -0600

    refactor: rewite PageHome.jsx as functional component; add commented PropTypes boilerplate

--- src/components/pages/PageHome.jsx
+++ src/components/pages/PageHome.jsx
@@ -1,28 +1,21 @@
-/* React */ 
 import React from 'react';
-
-/* Stylesheets */
+// import PropTypes from 'prop-types';
 import "../../styles/containers/PageHome.css";
 
-/* Assets */ 
+export const PageHome = props => {
+  const [] = React.useState();
 
-
-
-class PageHome extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <main>
-        <div>
-          <p>TwoFifteen is an open source database and API referencing all things related to professional baseball.  The data is comprehensive dating back to 1871 covering the ballparks (venues), franchises (teams), players and game details. </p>
-        </div>
-      </main>
-    );
-  }
+  return (
+    <main>
+      <div>
+        <p>TwoFifteen is an open source database and API referencing all things related to professional baseball.  The data is comprehensive dating back to 1871 covering the ballparks (venues), franchises (teams), players and game details. </p>
+      </div>
+    </main>
+  );
 }
+
+// PageHome.propTypes = {};
+
+// PageHome.defaultProps = {};
 
 export default PageHome
