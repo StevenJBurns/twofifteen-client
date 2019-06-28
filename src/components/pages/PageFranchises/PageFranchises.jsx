28e6079c8ca6b8a94ea2a1a4485595e8f26dda02
StevenJBurns <stevenjburns@msn.com>
2019-06-27 23:50:31 -0600

    refactor: rewrite PageFranchises from class component to functional component

--- src/components/pages/PageFranchises/PageFranchises.jsx
+++ src/components/pages/PageFranchises/PageFranchises.jsx
@@ -2,20 +2,14 @@ import React from 'react';
 // import { PropTypes } from 'prop-types';
 import './PageFranchises.scss';
 
-class PageFranchises extends React.Component {
-  
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+export const PageFranchises = props => {
+  const [] = React.useState();
 
-  render() {
-    return (
-      <main className="page-teams">
-        <div className="div-teams-content"></div>
-      </main>
-    );
-  }
+  return (
+    <main className="page-teams">
+      <div className="div-teams-content"></div>
+    </main>
+  );
 }
 
 export default PageFranchises;
