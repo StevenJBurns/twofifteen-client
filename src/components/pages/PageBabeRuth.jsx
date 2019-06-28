6df9694a63bacc62bfa10ab378a31d4dd7a18187
StevenJBurns <stevenjburns@msn.com>
2019-06-28 00:25:55 -0600

    refactor: rewrite PageBabeRuth as tiny functional component

--- src/components/pages/PageBabeRuth.jsx
+++ src/components/pages/PageBabeRuth.jsx
@@ -1,19 +1,5 @@
 import React from 'react';
 
-
-class PageBabeRuth extends React.Component {
-
-  constructor(props) {
-    super(props);
-
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <main></main>
-    );
-  }
-}
+export const PageBabeRuth  = props => (<main />);
 
 export default PageBabeRuth;
