8646ae8327d1c48b7cd3143c1e6bcbc46c272b58
StevenJBurns <stevenjburns@msn.com>
2019-06-27 23:46:36 -0600

    refactor: rename PageHome stylesheet from .css ro .scss

--- src/components/pages/PageHome/PageHome.jsx
+++ src/components/pages/PageHome/PageHome.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
 import { AppNav } from '../../app/AppNav/AppNav';
-import './PageHome.css';
+import './PageHome.scss';
 
 export const PageHome = props => {
   const [] = React.useState();
