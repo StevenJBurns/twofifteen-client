4d81505e5f4b99e200421972aa6fcbb3b5b68db7
StevenJBurns <stevenjburns@msn.com>
2019-06-28 00:23:06 -0600

    feat: create a SeasonSelect component; just basic boilerplate for now

---
+++ src/components/ui/SeasonSelect.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+// import { PropTypes } from 'prop-types';
+import './SeasonSelect.scss';
+
+export const SeasonSelect = props => {
+  return (
+    <ul className="season-select"></ul>
+  );
+};
+
+export default SeasonSelect;
