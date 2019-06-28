1ae2913b579c04e33816ac96ffe716523ce95acd
StevenJBurns <stevenjburns@msn.com>
2019-06-28 00:30:07 -0600

    refactor: rename base index.css file to index.scss

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './components/app/App/App.jsx';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './redux/store/';
-import './styles/index.css';
+import './styles/index.scss';
 
 ReactDOM.render(
   <Provider store={store}>
