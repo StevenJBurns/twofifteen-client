8ab9c728f8573c5295cb45e091baca8cdca9df1a
StevenJBurns <stevenjburns@msn.com>
2019-06-29 09:09:00 -0600

    refactor: rewrite some basics for OnThisDay component

--- src/components/ui/OnThisDay/OnThisDay.jsx
+++ src/components/ui/OnThisDay/OnThisDay.jsx
@@ -1,12 +1,14 @@
 import React from "react";
+import PropTypes from 'prop-types';
+import './OnThisDay.scss';
 
-
-export const OnThisDay = (props) => {
+export const OnThisDay = props => {
   const today = new Date().toDateString();
 
   return (
     <React.Fragment>
       <h3>{today}</h3>
+      <p></p>
     </React.Fragment>
   );
 }
