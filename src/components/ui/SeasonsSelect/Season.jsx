fc5decc178ed401b8fcdb027f1ad00e28e2685aa
StevenJBurns <stevenjburns@msn.com>
2019-07-01 09:11:26 -0600

    chore: probably unnecessaey with newer components but added React Router Link to the mix inside the Season component

--- src/components/ui/SeasonsSelect/Season.jsx
+++ src/components/ui/SeasonsSelect/Season.jsx
@@ -1,23 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import './Season.scss';
 
 export const Season = props => {
-  const { season, isSelected, toggleSeason } = props;
+  const { season, isSelected, toggleSeason, history, params } = props;
 
   // const [isSelected, setIsSelected] = React.useState(false);
+  console.log(props.match);
 
   const bgColor = isSelected ? 'green' : '#D0D0D0';
 
   const handleClick = e => {
     toggleSeason(season);
+    // history.push("")
     // setIsSelected(!isSelected);
   };
 
   return (
-    <li className="season-year" style={{backgroundColor: bgColor}} onClick={handleClick}>
-      {season}
-    </li>
+    <Link className="season-year__link" to={`/seasons/${season}`} component={null}>
+      <li className="season-year" style={{backgroundColor: bgColor}} onClick={handleClick}>
+        {season}
+      </li>
+    </Link>
   );
 };
 
