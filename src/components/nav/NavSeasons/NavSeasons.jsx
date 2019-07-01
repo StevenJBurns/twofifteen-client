f88e58236af6a91bb463708035eebb21342a2f70
StevenJBurns <stevenjburns@msn.com>
2019-07-01 09:14:19 -0600

    feat: new nav component NavSeasons for selecting season navigation over previous attempt

---
+++ src/components/nav/NavSeasons/NavSeasons.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { createSeasonsList } from '../../../utils/createSeasonsList';
+import './NavSeasons.scss';
+
+export const NavSeasons = () => {
+  return (
+    <>
+      <nav className="nav-season">
+          {
+            createSeasonsList().map(season =>
+              (<Link key={season} className="season" to={`/seasons/${season}`}>{season}</Link>))
+          }
+      </nav>
+      <Link className="nav-season__home-link" to='/seasons'>HOME</Link>
+    </>
+  );
+};
+
+export default NavSeasons;
