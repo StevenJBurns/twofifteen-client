8dad4eec8f50f81ec3903a61403098a9b11f10c2
StevenJBurns <stevenjburns@msn.com>
2019-07-01 09:10:24 -0600

    chore: clean up  Standings component a little more

--- src/components/ui/Standings/Standings.jsx
+++ src/components/ui/Standings/Standings.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import './SeasonStandings.scss';
+import './Standings.scss';
 
 export const SeasonStandings = props => {
-  const { season, leagues, divisions, teams } = props;
+  const { season, leagues } = props;
 
   return (
     <React.Fragment>
@@ -18,10 +18,12 @@ export const SeasonStandings = props => {
 
 SeasonStandings.propTypes = {
   season: PropTypes.number,
+  leagues: PropTypes.arrayOf(PropTypes.shape({})),
 };
 
 SeasonStandings.defaultProps = {
   season: new Date().getFullYear(),
+  leagues: [],
 };
 
 export default SeasonStandings;
