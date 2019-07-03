ddccf527a2e8791d61228c43e63e73d913a10a36
StevenJBurns <stevenjburns@msn.com>
2019-07-02 23:06:26 -0600

    feat: playing with PageSeasonDetail

--- src/components/pages/PageSeasonDetail/PageSeasonDetail.jsx
+++ src/components/pages/PageSeasonDetail/PageSeasonDetail.jsx
@@ -6,6 +6,10 @@ export const PageSeasonDetail = props => (
   <section className="page-season">
     <h2 className="page-season__h1">&larr; {props.match.params.season}</h2>
     <hr className="page-season__hr"></hr>
+    <h3>World Series</h3>
+    <h3>Attendance</h3>
+    <h3>All Star Game</h3>
+    <h3>Leaders</h3>
   </section>
 );
 
