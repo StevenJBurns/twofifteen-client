56df3759540536456b2b76671c6ebe9200f77b82
StevenJBurns <stevenjburns@msn.com>
2019-07-02 23:19:55 -0600

    chore: rename AppNav.css to .scss

--- src/components/app/AppNav/AppNav.jsx
+++ src/components/app/AppNav/AppNav.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import './AppNav.css';
+import './AppNav.scss';
 
 export const AppNav = props => {
   const [] = React.useState();
@@ -11,9 +11,9 @@ export const AppNav = props => {
       <ul className="app-nav__ul">
         <li><Link to="/">HOME</Link></li>
         <li><Link to="/seasons">SEASONS</Link></li>
-        <li><Link to="/ballparks">BALLPARKS and VENUES</Link></li>
+        <li><Link to="/venues">BALLPARKS and VENUES</Link></li>
         <li><Link to="/teams">TEAMS and FRANCHISES</Link></li>
-        <li><Link to="/players">PLAYERS</Link></li>
+        <li><Link to="/people">PEOPLE</Link></li>
         <li><Link to="/games">GAMES</Link></li>
         <li><Link to="/achievements">ACHIEVEMENTS and RECORDS</Link></li>
       </ul>
