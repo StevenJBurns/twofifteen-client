b4dd928b2beba0655c6e64038cdf9821a59df84f
StevenJBurns <stevenjburns@msn.com>
2019-07-05 15:37:31 -0600

    refactor: re-write AppFooter as functional component and add empty prop-types

--- src/components/app/AppFooter/AppFooter.jsx
+++ src/components/app/AppFooter/AppFooter.jsx
@@ -1,23 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './AppFooter.scss';
-class AppFooter extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+export const AppFooter = () => {
+  const year = new Date().getFullYear();
 
-  render() {
-    const year = new Date().getFullYear();
+  return (
+    <footer className="app-footer">
+      <h4>&copy; {year} Steven J Burns</h4>
+      <p>The information used here was obtained free of charge from and is copyrighted by Retrosheet.  Interested parties may contact Retrosheet at "www.retrosheet.org".</p>
+    </footer>
+  );
+};
 
-    return (
-      <footer className="App-footer">
-        <h4>&copy; {year} Steven J Burns</h4>
-        <p>The information used here was obtained free of charge from and is copyrighted by Retrosheet.  Interested parties may contact Retrosheet at "www.retrosheet.org".</p>
-      </footer>
-    );
-  }
-}
+AppFooter.propTypes = {};
+
+AppFooter.defaultProps = {};
 
 export default AppFooter;
