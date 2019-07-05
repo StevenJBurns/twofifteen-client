0d76c5b180b5e3fcf18bbecaa18dcf722b5fe9d7
StevenJBurns <stevenjburns@msn.com>
2019-07-05 15:53:03 -0600

    style: move ancient original AppFooter styling from App-1.css to AppFooter.scss

--- src/components/app/AppFooter/AppFooter.jsx
+++ src/components/app/AppFooter/AppFooter.jsx
@@ -6,9 +6,9 @@ export const AppFooter = () => {
   const year = new Date().getFullYear();
 
   return (
-    <footer className="app-footer">
-      <h4>&copy; {year} Steven J Burns</h4>
-      <p>The information used here was obtained free of charge from and is copyrighted by Retrosheet.  Interested parties may contact Retrosheet at "www.retrosheet.org".</p>
+    <footer id="app-footer">
+      <h4 className="app-footer__h4">&copy; {year} &bull; Steven J Burns</h4>
+      <p app-footer="app-footer_p">The information used here was obtained free of charge from and is copyrighted by Retrosheet.  Interested parties may contact Retrosheet at "www.retrosheet.org".</p>
     </footer>
   );
 };
