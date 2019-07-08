0cdddfd79e4fe0593b6bb2fa4b53292157a900d2
StevenJBurns <stevenjburns@msn.com>
2019-07-07 23:14:55 -0600

    refactor: add BEM context to classNames and styles in AppFooter

--- src/components/app/AppFooter/AppFooter.jsx
+++ src/components/app/AppFooter/AppFooter.jsx
@@ -7,8 +7,8 @@ export const AppFooter = () => {
 
   return (
     <footer id="app-footer">
-      <h4 className="app-footer__h4">&copy; {year} &bull; Steven J Burns</h4>
       <p app-footer="app-footer_p">The information used here was obtained free of charge from and is copyrighted by Retrosheet.  Interested parties may contact Retrosheet at "www.retrosheet.org".</p>
+      <h4 className="app-footer__h4">&copy; {year} &bull; Steven J Burns</h4>
     </footer>
   );
 };
