3a4820d61ac0f31b807158146927c6bd90699420
StevenJBurns <stevenjburns@msn.com>
2019-11-22 03:24:27 -0700

    refactor: add Router hooks to PageSeasonDetail

--- src/components/pages/PageSeasonDetail/PageSeasonDetail.jsx
+++ src/components/pages/PageSeasonDetail/PageSeasonDetail.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { NavLink, useParams, useHistory } from 'react-router-dom';
 import './PageSeasonDetail.scss';
 
-export const PageSeasonDetail = props => (
-  <section className="page-season">
-    <h2 className="page-season__h1">&larr; {props.match.params.season}</h2>
-    <hr className="page-season__hr"></hr>
-    <h3>World Series</h3>
-    <h3>Attendance</h3>
-    <h3>All Star Game</h3>
-    <h3>Leaders</h3>
-  </section>
-);
+export const PageSeasonDetail = props => {
+  const params = useParams();
+  const history = useHistory();
+  console.log('props: ', props);
+  console.log('params: ', params);
+  
+  return (
+    <section className="page-season">
+      <h2 className="page-season__h1" onClick={() => history.goBack()}>
+        &larr; {params.season}
+      </h2>
+      <hr className="page-season__hr"></hr>
+      <h3>World Series</h3>
+      <h3>Attendance</h3>
+      <h3>All Star Game</h3>
+      <h3>Leaders</h3>
+    </section>
+  );
+};
 
 export default PageSeasonDetail;
