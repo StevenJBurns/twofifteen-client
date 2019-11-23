9b40f0f3275d097f9d9f4fdd383a2291f5134eb7
StevenJBurns <stevenjburns@msn.com>
2019-11-22 23:53:49 -0700

    chore: re-order import statements in index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/app/App/App.jsx';
-import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import App from './components/app/App/App.jsx';
 import { store } from './redux/store/';
 import './index.scss';
 
