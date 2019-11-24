533d77710f707f1840aed3a2902693d24b8f8641
StevenJBurns <stevenjburns@msn.com>
2019-11-24 08:51:41 -0700

    feat: create reusable PageHeader to be used with generic Page

---
+++ src/components/pages/Page/PageHeader.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const PageHeader = props => {
+  return (
+    <header style={{ color:'#D0D0D0', textAlign: 'center'}}>
+      <h1>TWO-FIFTEEN</h1>
+      <hr />
+    </header>
+  );
+};
+
+export default PageHeader;
