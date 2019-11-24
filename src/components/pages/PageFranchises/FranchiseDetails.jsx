6edcc4c9c4292d40c4100ed5a516d1c30381726c
StevenJBurns <stevenjburns@msn.com>
2019-11-24 07:07:12 -0700

    feat: PageFtanchiseDeatail now correctly loads JSON async

--- src/components/pages/PageFranchises/FranchiseDetails.jsx
+++ src/components/pages/PageFranchises/FranchiseDetails.jsx
@@ -1,27 +1,23 @@
 import React from 'react';
-import { useHistory, useParams, useLocation } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import franchiseData from '../../../data/franchises.json';
 
-export const PageFranchiseDetail = props => {
-  const location = useLocation();
-  const history = useHistory();
-  const params = useParams();
-
-  const [franchiseObj, setFranchiseObj] = React.useState(null);
+export const PageFranchiseDetail = () => {
+  const [franchiseObj, setFranchiseObj] = React.useState({});
+  const { id } = useParams();
 
   React.useEffect(() => {
-    if (!franchiseObj) setFranchiseObj(findFranchise(params.id));
-  });
+    const findFranchise = async id => {
+      const franchise = await franchiseData.find(f => f.id === +id);
+      setFranchiseObj(franchise);
+    };
 
-  const findFranchise = async id => {
-    await franchiseData.find(f => f.id === params.id);
-  }
-
-  console.log('franchiseObj: ', franchiseObj);
+    findFranchise(id) 
+  }, [id, franchiseObj, setFranchiseObj]);
   
   return (
     <div style={{ textAlign: 'center' }}>
-      <h1 style={{ color: '#D0D0D0' }}>Franchise Detail:</h1>
+      <h1 style={{ color: '#D0D0D0' }}>{franchiseObj.name}</h1>
     </div>
   );
 };
