1288d6ff0c8d338aedcd19b550d964dfe79eac48
StevenJBurns <stevenjburns@msn.com>
2019-11-26 22:50:10 -0700

    refactor: create PageFooter and PageNav components vs my old Appnav and AppFooter flow

--- src/components/pages/Page/PageFooter.jsx
+++ src/components/pages/Page/PageFooter.jsx
@@ -1,16 +1,46 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/styles';
+import { flexbox } from '@material-ui/system';
+
+const useStyles = makeStyles({
+  footer: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    minHeight: '8rem',
+    color: "#D0D0D0",
+    backgroundColor: 'darkblue',
+    textAlign: 'center',
+  },
+  p: {
+    margin: '1rem auto',
+    display: 'inline-block',
+    minWidth: 280,
+    maxWidth: 600,
+  },
+  h4: {
+    display: 'inline-block',
+    margin: '1rem auto',
+  },
+  hr : {
+    width: '100%',
+    minWidth: 280,
+  },
+})
 
 export const PageFooter = () => {
+  const styles = useStyles();
   const year = new Date().getFullYear();
 
   return (
-    <footer style={{ color: "#D0D0D0", textAlign: 'center'}}>
-      <p app-footer="app-footer_p">The information used here was obtained free of charge from and is copyrighted by Retrosheet.  Interested parties may contact Retrosheet at&nbsp;
+    <footer className={styles.footer}>
+      <p className={styles.p}>The information used here was obtained free of charge from and is copyrighted by Retrosheet.  Interested parties may contact Retrosheet at&nbsp;
         <a href="www.retrosheet.org" style={{margin: 'inherit'}}>www.retrosheet.org</a>.
       </p>
-      <hr />
-      <h4 className="app-footer__h4">&copy; {year} &bull; Steven J Burns</h4>
+      <hr className={styles.hr}></hr>
+      <h4 className={styles.h4}>&copy; {year} &bull; Steven J Burns</h4>
     </footer>
   )
 };
--- src/components/pages/Page/PageNav.jsx
+++ src/components/pages/Page/PageNav.jsx
@@ -5,12 +5,12 @@ import { NavLink } from 'react-router-dom';
 export const PageNav = props => {
   return (
     <nav style={{ color: "#D0D0D0", textAlign: 'center'}}>
-      <h2>Site Navigation</h2>
       <ul>
         <NavLink to="/">HOME</NavLink>
-        <NavLink to="/franchises">TEAMS</NavLink>
         <NavLink to="/venues">VENUES</NavLink>
-        <NavLink to="/seasons">SEASON</NavLink>
+        <NavLink to="/franchises">TEAMS</NavLink>
+        <NavLink to="/seasons">SEASONS</NavLink>
+        <NavLink to="/games">GAMES</NavLink>
       </ul>
       <hr></hr>
     </nav>
